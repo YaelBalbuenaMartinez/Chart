47ad82e218cad9896092596c841b2d957ff6dc7d
YaelBalbuenaMartinez <yael_1321124280@uptecamac.edu.mx>
2023-09-27 15:16:24 -0600

    Fix(ejemplo2): View fix and add more graphics

--- resources/views/ejemplo2.blade.php
+++ resources/views/ejemplo2.blade.php
@@ -1,39 +1,28 @@
-<!-- 
-<table>
-    <tr>
-        <td>COL1</td>
-        <td>COL2</td>
-        <td>COL3</td>
-        <td>COL4</td>
-    </tr>
-    @foreach ($completas as $completa)
-    <tr>
-        <td>{{ $completa->id }}</td>
-        <td>{{ $completa->estatus_vigente }}</td>
-        <td>{{ $completa->estado_medico }}</td>
-        <td>{{ $completa->municipio_medico }}</td>
-    </tr>
-    @endforeach
-</table> -->
+
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
 <canvas id="chartEstatus" width="300" height="300"></canvas>
 
 <canvas id="chartNomInst" width="400" height="400"></canvas>
 
-<canvas id="chartMunicipio" width="400" height="400"></canvas>
+<!-- <canvas id="chartMunicipio" width="400" height="400"></canvas> -->
 
+<canvas id="chartEstMedic" width="400" height="400"></canvas>
+
+<canvas id="chartEdad" width="800" height="400"></canvas>
+
+<canvas id="chartGenero" width="300" height="300"></canvas>
 
 <!-- script para el chart de vigencia de estatus -->
 <script>
     const estatus = {!! json_encode($completas->pluck('estatus_vigente')->all()) !!};
     const estatusCounts = {};
 
-    estatus.forEach((estatus) => {
-    if (estatusCounts[estatus]) {
-        estatusCounts[estatus]++;
+    estatus.forEach((estat) => {
+    if (estatusCounts[estat]) {
+        estatusCounts[estat]++;
     } else {
-        estatusCounts[estatus] = 1;
+        estatusCounts[estat] = 1;
     }
 });
 
@@ -71,21 +60,21 @@
     const nomIns = {!! json_encode($completas->pluck('nombre_institucion')->all()) !!};
     const nomInsCounts = {};
 
-    nomIns.forEach((nomIns) => {
-    if (nomInsCounts[nomIns]) {
-        nomInsCounts[nomIns]++;
+    nomIns.forEach((nomIn) => {
+    if (nomInsCounts[nomIn]) {
+        nomInsCounts[nomIn]++;
     } else {
-        nomInsCounts[nomIns] = 1;
+        nomInsCounts[nomIn] = 1;
     }
 });
 
 
     const nomInsLabels = Object.keys(nomInsCounts);
-    const nomInsData = estatusLabels.map((label) => nomInsCounts[label]);
+    const nomInsData = nomInsLabels.map((label) => nomInsCounts[label]);
 
     const ctxnomIns = document.getElementById('chartNomInst').getContext('2d');
         const chartnomIns = new Chart(ctxnomIns, {
-            type: 'doughnut',
+            type: 'bar',
             data: {
                 labels: nomInsLabels,
                 datasets: [{
@@ -109,4 +98,166 @@
 </script>
 
 
+<!-- chart para los municipios -->
 
+<!-- <script>
+    const Municipios = {!! json_encode($completas->pluck('municipio_medico')->all()) !!};
+    const MunicipiosCount = {};
+
+    Municipios.forEach((Municipio) => {
+    if (MunicipiosCount[Municipio]) {
+        MunicipiosCount[Municipio]++;
+    } else {
+        MunicipiosCount[Municipio] = 1;
+    }
+});
+
+
+    const MunicipioLabels = Object.keys(MunicipiosCount);
+    const MunicipioData = MunicipioLabels.map((label) => MunicipiosCount[label]);
+
+    const ctxMun = document.getElementById('chartMunicipio').getContext('2d');
+        const chartMun = new Chart(ctxMun, {
+            type: 'bar',
+            data: {
+                labels: MunicipioLabels,
+                datasets: [{
+                    label: 'Cantidad de municipios medicos',
+                    data: MunicipioData,
+                    backgroundColor: ["#FF0000", "#00FF00", '#FF59E3', '#0000FF', '#FFA500', '#FFFF00', '#FF00FF', '#00FFFF', '#800080', '#008000', '#800000', '#808080', '#008080'],
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    
+</script> -->
+
+<script>
+    const EstMedic = {!! json_encode($completas->pluck('estado_medico')->all()) !!};
+    const EstMedicCount = {};
+
+    EstMedic.forEach((EstMedics) => {
+    if (EstMedicCount[EstMedics]) {
+        EstMedicCount[EstMedics]++;
+    } else {
+        EstMedicCount[EstMedics] = 1;
+    }
+});
+
+
+    const EstMedicLabels = Object.keys(EstMedicCount);
+    const EstMedicData = EstMedicLabels.map((label) => EstMedicCount[label]);
+
+    const ctxMedic = document.getElementById('chartEstMedic').getContext('2d');
+        const chartMedic = new Chart(ctxMedic, {
+            type: 'line',
+            data: {
+                labels: EstMedicLabels,
+                datasets: [{
+                    label: 'Cantidad de municipios medicos',
+                    data: EstMedicData,
+                    backgroundColor: ["#FF0000", "#00FF00", '#FF59E3', '#0000FF', '#FFA500', '#FFFF00', '#FF00FF', '#00FFFF', '#800080', '#008000', '#800000', '#808080', '#008080'],
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+    
+</script>
+
+<script>
+    const Edades = {!! json_encode($completas->pluck('edad')->all()) !!};
+    const EdadesCounts = {};
+
+    Edades.forEach((Edad) => {
+    if (EdadesCounts[Edad]) {
+        EdadesCounts[Edad]++;
+    } else {
+        EdadesCounts[Edad] = 1;
+    }
+});
+
+
+    const EdadesLabels = Object.keys(EdadesCounts);
+    const EdadesData = EdadesLabels.map((label) => EdadesCounts[label]);
+
+    const ctxEdad = document.getElementById('chartEdad').getContext('2d');
+        const chartEdad = new Chart(ctxEdad, {
+            type: 'line',
+            data: {
+                labels: EdadesLabels,
+                datasets: [{
+                    label: 'Cantidad de edades',
+                    data: EdadesData,
+                    backgroundColor: ["#FF0000", "#00FF00", '#FF59E3', '#0000FF', '#FFA500', '#FFFF00', '#FF00FF', '#00FFFF', '#800080', '#008000', '#800000', '#808080', '#008080'],
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+</script>
+
+<script>
+    const generos = {!! json_encode($completas->pluck('Genero')->all()) !!};
+        const generoCounts = {};
+
+        generos.forEach((genero) => {
+            if (generoCounts[genero]) {
+                generoCounts[genero]++;
+            } else {
+                generoCounts[genero] = 1;
+            }
+        });
+
+        const generoLabels = Object.keys(generoCounts);
+        const generoData = generoLabels.map((label) => generoCounts[label]);
+
+        // Crear el gráfico
+        const ctxGen = document.getElementById('chartGenero').getContext('2d');
+        const chartGen = new Chart(ctxGen, {
+            type: 'doughnut',
+            data: {
+                labels: generoLabels,
+                datasets: [{
+                    label: 'Cantidad por Género',
+                    data: generoData,
+                    backgroundColor: ["#FF6384", "#36A2EB"],
+                    borderColor: 'rgba(54, 162, 235, 1)',
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                responsive: false,
+                scales: {
+                    y: {
+                        beginAtZero: true
+                    }
+                }
+            }
+        });
+</script>
